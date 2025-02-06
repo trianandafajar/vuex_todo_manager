c7868dc4d1a63c25d5c64503fb9d91b41c673b99
trianandafajar <mail.trianandafajar@gmail.com>
2025-02-06 10:02:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    <div class="container">
+      <h1>Test</h1>
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="">
+    <h3>Todos</h3>
+    <div class="todos">
+        <div class="todo" v-for="todo in allTodos" :key="todo.id">{{ todo.title }}</div>
+    </div>
+</div>  
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    name: "TodosComponent",
+    computed: mapGetters(['allTodos'])
+}
+</script>
+
+<style>
+
+</style>
