33a2b307e66a936f7804a2b9233915a690bf6ed1
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-10 10:02:52 +0700

    Update Index

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
-import { createStore } from "vuex";
-import todos from "./modules/todos";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import todos from './modules/todos'
 
-export default createStore({
-  module: {
-    todos,
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    todos
   },
-});
+  strict: process.env.NODE_ENV !== 'production'
+})
