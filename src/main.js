fc7b152500310565447b4af46dc9d1f51f523b41
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-10 10:02:23 +0700

    Update Main Js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import store from './store';
+import store from './store'
+import './assets/styles/main.css'
 
-createApp(App)
-    .use(store)
-    .mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
