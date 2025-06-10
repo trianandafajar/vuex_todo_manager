240b3a259876d1b1d4d5c12384cb17d29dd24b9d
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-10 10:02:10 +0700

    Update Todos

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,36 +1,50 @@
 // import axios from 'axios';
 
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: "Todo One",
-    },
-    {
-      id: 2,
-      title: "Todo Two",
-    },
-  ],
+  todos: []
 };
 
 const getters = {
-  allTodos: (state) => state.todos,
+  allTodos: state => state.todos,
+  completedTodos: state => state.todos.filter(todo => todo.completed),
+  activeTodos: state => state.todos.filter(todo => !todo.completed)
 };
 
 const actions = {
   addTodo({ commit }, title) {
-    const newTodo = {
-      id: Date.now(), // pakai timestamp sebagai id
+    const todo = {
+      id: Date.now(),
       title,
+      completed: false,
+      createdAt: new Date().toISOString()
     };
-    commit("ADD_TODO", newTodo);
+    commit('ADD_TODO', todo);
   },
+  
+  toggleTodo({ commit }, id) {
+    commit('TOGGLE_TODO', id);
+  },
+  
+  deleteTodo({ commit }, id) {
+    commit('DELETE_TODO', id);
+  }
 };
 
 const mutations = {
   ADD_TODO(state, todo) {
-    state.todos.push(todo);
+    state.todos.unshift(todo);
   },
+  
+  TOGGLE_TODO(state, id) {
+    const todo = state.todos.find(todo => todo.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+    }
+  },
+  
+  DELETE_TODO(state, id) {
+    state.todos = state.todos.filter(todo => todo.id !== id);
+  }
 };
 
 export default {
@@ -38,5 +52,5 @@ export default {
   state,
   getters,
   actions,
-  mutations,
+  mutations
 };
